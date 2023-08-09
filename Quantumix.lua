8bc591b175a1c008f1b8c630aab479016845329f
ZenithDust <138144854+ZenithDust@users.noreply.github.com>
2023-08-10 01:55:08 +0800

    Update Quantumix.lua

--- Quantumix.lua
+++ Quantumix.lua
@@ -1,7 +1,7 @@
 local HttpRequest = game:GetService('HttpService')
 local WebsiteUrl = "https://quantumixwebsite.zenithdust.repl.co/increment_players"
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/ZenithDust/Quantumix/main/Games/13127800756.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/ZenithDust/Quantumix/main/Games/QXAWS.lua'))()
 
 local reponse = {Url=WebsiteUrl, Body="", Method="POST", Headers="application/json"}
 local request = http_request or request or HttpPost or syn.request
