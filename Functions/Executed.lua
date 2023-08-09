c683aeebcb0df0b603c3d184d19a3006ca452813
ZenithDust <138144854+ZenithDust@users.noreply.github.com>
2023-08-10 01:59:57 +0800

    Update Executed.lua

--- Functions/Executed.lua
+++ Functions/Executed.lua
@@ -1,1 +1,10 @@
+local HttpRequest = game:GetService('HttpService')
+local WebsiteUrl = "https://quantumixwebsite.zenithdust.repl.co/increment_players"
 
+local reponse = {Url=WebsiteUrl, Body="", Method="POST", Headers="application/json"}
+local request = http_request or request or HttpPost or syn.request
+
+local r = request(reponse)
+for i,v in pairs(r) do
+  print(v)
+end
