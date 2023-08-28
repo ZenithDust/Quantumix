a97238632bdfe6575c5499822f143f7a584f13c2
ZenithDust <138144854+ZenithDust@users.noreply.github.com>
2023-08-29 06:49:36 +0800

    Update Services.lua

--- Functions/Services.lua
+++ Functions/Services.lua
@@ -1,4 +1,4 @@
-getgenv().GameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name,
-getgenv().HttpService = game:GetService("HttpService"),
-getgenv().Players = game:GetService('Players'),
-getgenv().vu = game:GetService('VirtualUser')
+GameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name,
+HttpService = game:GetService("HttpService"),
+Players = game:GetService('Players'),
+vu = game:GetService('VirtualUser')
