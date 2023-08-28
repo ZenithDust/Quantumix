de4a09ea527f52bdfe21f8d8d033cea945a2afb1
ZenithDust <138144854+ZenithDust@users.noreply.github.com>
2023-08-29 06:22:00 +0800

    Create Services.lua

---
+++ Functions/Services.lua
@@ -0,0 +1,4 @@
+local GameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+local HttpService = game:GetService("HttpService")
+local Players = game:GetService('Players')
+local vu = game:GetService('VirtualUser')
