fbbda8be805281c07f2ec2cbffddddc20a4e227e
ZenithDust <138144854+ZenithDust@users.noreply.github.com>
2023-08-29 06:44:34 +0800

    Update Services.lua

--- Functions/Services.lua
+++ Functions/Services.lua
@@ -1,6 +1,4 @@
-local customs = {
-  ['GameName'] = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name,
-  ['HttpService'] = game:GetService("HttpService"),
-  ['Players'] = game:GetService('Players'),
-  ['vu'] = game:GetService('VirtualUser')
-}
+getgenv().GameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name,
+getgenv().HttpService = game:GetService("HttpService"),
+getgenv().Players = game:GetService('Players'),
+getgenv().vu = game:GetService('VirtualUser')
