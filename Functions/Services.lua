7272489530e4a730e9a13ca5329c5d47d0856d70
cornoriddle@gmail.com <cornoriddle@gmail.com>
2023-09-03 21:39:50 +0800

    Pushed Another One

--- Functions/Services.lua
+++
@@ -1,4 +0,0 @@
-GameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name,
-HttpService = game:GetService("HttpService"),
-Players = game:GetService('Players'),
-vu = game:GetService('VirtualUser')
