a26c1e8290f55f396133a625f796e5a0603d044d
cornoriddle@gmail.com <cornoriddle@gmail.com>
2023-09-10 08:17:03 +0800

    Witty

--- Functions/Executed.lua
+++ Functions/Executed.lua
@@ -1,5 +1,5 @@
 local HttpRequest = game:GetService('HttpService')
-local WebsiteUrl = "https://quantumixwebsite.zenithdust.repl.co/increment_players"
+local WebsiteUrl = "http://hub.quantumix.fun/api/quantumix/executed"
 
 local reponse = {Url=WebsiteUrl, Body="", Method="POST", Headers="application/json"}
 local request = http_request or request or HttpPost or syn.request
