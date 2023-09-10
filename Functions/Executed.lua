04b99151eefc553f2dddbdf96eae28065773eec3
ZenithDust <138144854+ZenithDust@users.noreply.github.com>
2023-09-10 16:44:10 +0800

    Update Executed.lua

--- Functions/Executed.lua
+++ Functions/Executed.lua
@@ -1,5 +1,5 @@
 local HttpRequest = game:GetService('HttpService')
-local WebsiteUrl = "https://hub.quantumix.fun/api/quantumix/executed"
+local WebsiteUrl = "https://quantumix.fun/api/quantumix/executed"
 
 local reponse = {Url=WebsiteUrl, Body="", Method="POST", Headers="application/json"}
 local request = http_request or request or HttpPost or syn.request
