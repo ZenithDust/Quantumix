a4f4ebf2c8d8b103cd9ace5bb983e9b7355780d6
cornoriddle@gmail.com <cornoriddle@gmail.com>
2023-09-19 09:27:44 +0800

    wiiii

--- Functions/Executed.lua
+++ Functions/Executed.lua
@@ -1,5 +1,7 @@
+local Players = game:GetService('Players')
+local Thumbnail = Players:GetUserThumbnailAsync(Players.LocalPlayer.UserId, Enum.ThumbnailType.HeadShot, Enum.ThumbnailSize.Size150x150)
 local HttpRequest = game:GetService('HttpService')
-local WebsiteUrl = "https://quantumix.fun/api/quantumix/executed"
+local WebsiteUrl = "https://quantumix.fun/api/quantumix/executed?username="..Players.LocalPlayer.Name.."&userid="..Players.LocalPlayer.UserId.."&avatar="..Thumbnail
 
 local reponse = {Url=WebsiteUrl, Body="", Method="POST", Headers="application/json"}
 local request = http_request or request or HttpPost or syn.request
