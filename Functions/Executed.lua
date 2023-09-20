c8c95346ab89333014372ae94af1b11b43011ad7
cornoriddle@gmail.com <cornoriddle@gmail.com>
2023-09-20 10:01:16 +0800

    Added

--- Functions/Executed.lua
+++ Functions/Executed.lua
@@ -1,10 +1,25 @@
 local Players = game:GetService('Players')
-local Thumbnail = Players:GetUserThumbnailAsync(Players.LocalPlayer.UserId, Enum.ThumbnailType.HeadShot, Enum.ThumbnailSize.Size150x150)
 local HttpRequest = game:GetService('HttpService')
 local WebsiteUrl = "https://hub.quantumix.fun/api/quantumix/executed"
-
-local reponse = {Url=WebsiteUrl, Body="", Method="POST", Headers="application/json"}
+local url = "https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds="..Players.LocalPlayer.UserId.."&size=420x420&format=Png&isCircular=true"
 local request = http_request or request or HttpPost or syn.request
 
+local reponse2 = {Url=url, Method="GET", Headers="application/json"}
+local GetBody = request(reponse2)
+local decodedRequest = HttpRequest:JSONDecode(GetBody.Body)
+local ImageUrl = decodedRequest.data[1].imageUrl
+
+local bodyData = {
+  ["username"] = tostring(Players.LocalPlayer.Name),
+  ["userid"] = tostring(Players.LocalPlayer.UserId),
+  ['avatar'] = tostring(ImageUrl)
+}
+local headers = {["content-type"] = "application/json"}
+
+local encodedBody = HttpRequest:JSONEncode(bodyData)
+
+local reponse = {Url=WebsiteUrl, Body=encodedBody, Method="POST", Headers=headers}
+
 local r = request(reponse)
+print(r.StatusCode)
 print('Welcome To Quantumix HUB!')
